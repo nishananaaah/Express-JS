81ce146abf6e4310c0fa19c684e46d1366d7f890
nishananaaah <nishanaf731@gmail.com>
2024-12-24 11:31:53 +0530

    task

--- task.js
+++ task.js
@@ -8,6 +8,20 @@ app.post('/submit',(req,res)=>{
     const user = req.body
     res.send(user)
 })
+const token = (req,res,next)=>{
+    console.log("token is created")
+    next()
+}
+app.get('/submit',token,(req,res)=>{
+    res.send("user is nishana")
+})
+app.put('/submit/update',(req,res)=>{
+    res.send("user updated")
+})
+app.delete('/submit/delete',(req,res)=>{
+    res.send("user deleted")
+})
+
 app.listen(port,()=>{
     console.log("post")
 })
